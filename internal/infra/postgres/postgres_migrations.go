af5c73024d0b1a42b3acf203a5625e338c4abb84
Ashef Tishad <ashef.swe@gmail.com>
2024-10-07 04:04:08 +0600

    feat(postgres): Add connection and migration handlers
    - Implement NewConnection for pgx-based DB setup with pool configuration
    - Create RunMigrations function using golang-migrate/v4

---
+++ internal/infra/postgres/postgres_migrations.go
@@ -0,0 +1,34 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log/slog"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+// RunMigrations executes database migrations using the golang-migrate/v4 library.
+func RunMigrations(ctx context.Context, db *sql.DB, l *slog.Logger) error {
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to create database driver: %w", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://migrations",
+		"postgres", driver)
+
+	if err != nil {
+		return fmt.Errorf("failed to create new migrate instance: %w", err)
+	}
+
+	if err = m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("failed to run migrations: %w", err)
+	}
+
+	return nil
+}
