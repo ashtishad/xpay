1c9e84b2fe88762e75a655b0a90dde79e6795bb8
Ashef Tishad <ashef.swe@gmail.com>
2024-10-15 10:29:49 +0600

    Update

--- scripts/setup-dev-env.sh
+++ scripts/setup-dev-env.sh
@@ -2,11 +2,11 @@
 
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
-cp "$PROJECT_ROOT/local-dev/Makefile.dev" "$PROJECT_ROOT/Makefile"
-cp "$PROJECT_ROOT/local-dev/compose.yaml.dev" "$PROJECT_ROOT/compose.yaml"
+cp "$PROJECT_ROOT/env-configs/Makefile.dev" "$PROJECT_ROOT/Makefile"
+cp "$PROJECT_ROOT/env-configs/compose.yaml.dev" "$PROJECT_ROOT/compose.yaml"
 
 if [ ! -f "$PROJECT_ROOT/config.yaml" ]; then
-    cp "$PROJECT_ROOT/local-dev/config.yaml.example" "$PROJECT_ROOT/config.yaml"
+    cp "$PROJECT_ROOT/env-configs/config.yaml.example" "$PROJECT_ROOT/config.yaml"
 fi
 
 # Function to install a Go package if it's not already installed
--- scripts/setup-prod-env.sh
+++ scripts/setup-prod-env.sh
@@ -2,11 +2,11 @@
 
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
-cp "$PROJECT_ROOT/local-dev/Makefile.prod" "$PROJECT_ROOT/Makefile"
-cp "$PROJECT_ROOT/local-dev/compose.yaml.prod" "$PROJECT_ROOT/compose.yaml"
+cp "$PROJECT_ROOT/env-configs/Makefile.prod" "$PROJECT_ROOT/Makefile"
+cp "$PROJECT_ROOT/env-configs/compose.yaml.prod" "$PROJECT_ROOT/compose.yaml"
 
 if [ ! -f "$PROJECT_ROOT/config.yaml" ]; then
-    cp "$PROJECT_ROOT/local-dev/config.yaml.example" "$PROJECT_ROOT/config.yaml"
+    cp "$PROJECT_ROOT/env-configs/config.yaml.example" "$PROJECT_ROOT/config.yaml"
 fi
 
 echo "Production environment set up complete!"
